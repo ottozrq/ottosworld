c1beb3b192b1e6aabe7b9212ac5d8c3080a2beef
Ruoqiu ZHANG <ottozrq@gmail.com>
2025-08-01 20:59:32 +0200

    feat: add snoji policy page

--- ottosworld.py
+++ ottosworld.py
@@ -49,6 +49,21 @@ def it():
     return render_template('it.html')
 
 
+@app.route("/it/policy/snoji/en")
+def snoji_policy_en():
+    return render_template('policy/snoji_en.html')
+
+
+@app.route("/it/policy/snoji/zh")
+def snoji_policy_zh():
+    return render_template('policy/snoji_zh.html')
+
+
+@app.route("/it/policy/snoji/fr")
+def snoji_policy_fr():
+    return render_template('policy/snoji_fr.html')
+
+
 # Page for about
 @app.route("/about")
 def okr():
